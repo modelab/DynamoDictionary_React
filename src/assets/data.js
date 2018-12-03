7ee21c59e156b94d5fd8036387632112b8d13518
Scott Mitchell <scott.a.mitchell13@gmail.com>
2018-12-03 16:36:09 -0500

    Update Revit Nodes json (#4)
    * Updated xml and json data for Dynamo 2.0
    
    * moved old xml and json files
    
    * removed unnecessary diffs
    
    * revert IMAGE_FALSE_AVP_TRUE to true.
    
    * added Revit nodes to 2.0 update
    
    * Revert to previously migrated Dynamo_Nodes_Documentation
    
    * Removed AVP
    
    * updated Revit nodes json

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,7 +4,7 @@ import p from "../util/promise";
 //create promises for two data files to be read
 const pEdit = p.promisify(d3.json, "data/Dynamo_Nodes_Documentation.json");
 const pRevit = p.promisify(d3.xml, "data/Revit_Library.xml");
-const pBacklog = p.promisify(d3.json, "data/ExampleFileBacklog.json");
+const pBacklog = p.promisify(d3.json, "data/Dynamo_Nodes_Revit.json");
 
 //resolve promises
 export default Promise.all([pRevit, pEdit, pBacklog]);
